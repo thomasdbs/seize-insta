a33f8f7da7cdef93fa876e7461c50cb489250e73
Thomas Dubois <thomas.dubois@agenceseize.fr>
2018-11-14 11:36:51 +0100

    Updates

---
+++ precache-manifest.bf37e4b9ad6323db1cd73a7e9925de4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb1a993cbc15dc31162d",
+    "url": "/seize-insta/static/js/runtime~main.fb1a993c.js"
+  },
+  {
+    "revision": "10d7a2f429c0229264de",
+    "url": "/seize-insta/static/js/main.10d7a2f4.chunk.js"
+  },
+  {
+    "revision": "d6e1ebf3cdf5b629432a",
+    "url": "/seize-insta/static/js/1.d6e1ebf3.chunk.js"
+  },
+  {
+    "revision": "10d7a2f429c0229264de",
+    "url": "/seize-insta/static/css/main.965354e9.chunk.css"
+  },
+  {
+    "revision": "d6e1ebf3cdf5b629432a",
+    "url": "/seize-insta/static/css/1.39e46948.chunk.css"
+  },
+  {
+    "revision": "b2f321a590a12e2cf50293fc7cce4b26",
+    "url": "/seize-insta/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/seize-insta/precache-manifest.bf37e4b9ad6323db1cd73a7e9925de4f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/seize-insta/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
